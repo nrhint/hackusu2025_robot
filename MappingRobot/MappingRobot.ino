fa93a05634af78d205403cf15009601d91558229
Trent Chipman <trentchip@gmail.com>
2025-02-28 23:29:37 -0700

    Made custom motor stepping function

--- MappingRobot/MappingRobot.ino
+++ MappingRobot/MappingRobot.ino
@@ -23,8 +23,8 @@ Used:
  * by Isaac100
  */
  //Ultrasonic Sensors
-const int trigPin = 9;
-const int echoPin = 10;
+const int trigPin = 12;
+const int echoPin = 13;
 float duration_1, distance_1;
 
 int SonicSense(){
@@ -47,30 +47,98 @@ const int stepsPerRevolution = 200;  // change this to fit the number of steps p
 
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+const int MotorPinsRight[] = {8, 9, 10, 11};
+const int MotorPinsLeft[] = {7, 6, 5, 4};
+int Time_Delay = 3;//3 milliseconds
+void MoveSide(int motor[], int steps){
+  for (int j = 0; j < steps; j++){
+    for (int i = 0; i < 4; i++){
+      digitalWrite(motor[i], HIGH);
+      delay(Time_Delay);
+      digitalWrite(motor[i], LOW);
+    }
+  }
+}
+void MoveForward(int steps){
+  for (int j = 0; j < steps; j++){
+    for (int i = 0; i < 4; i++){
+      digitalWrite(MotorPinsRight[i], HIGH);
+      digitalWrite(MotorPinsLeft[i], HIGH);
+      delay(Time_Delay);
+      digitalWrite(MotorPinsRight[i], LOW);
+      digitalWrite(MotorPinsLeft[i], LOW);
+    }
+  }
+}
+void TurnLeft(int steps){
+  for (int n = 0; n < steps; n++){
+    int j = 3;
+    for (int i = 0; i < 4; i++){
+      digitalWrite(MotorPinsRight[j], HIGH);
+      digitalWrite(MotorPinsLeft[i], HIGH);
+      delay(Time_Delay);
+      digitalWrite(MotorPinsRight[j], LOW);
+      digitalWrite(MotorPinsLeft[i], LOW);
+      j -= 1;
+    }
+  }
+}
+void TurnRight(int steps){
+  for (int n = 0; n < steps; n++){
+    int j = 3;
+    for (int i = 0; i < 4; i++){
+      digitalWrite(MotorPinsRight[i], HIGH);
+      digitalWrite(MotorPinsLeft[j], HIGH);
+      delay(Time_Delay);
+      digitalWrite(MotorPinsRight[i], LOW);
+      digitalWrite(MotorPinsLeft[j], LOW);
+      j -= 1;
+    }
+  }
+}
+//Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
 
 int stepCount = 0;  // number of steps the motor has taken
 
 void setup() {
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  Serial.begin(9600);
+  //First motor pin initializations
+  for (int i = 0; i < 4; i++){
+    pinMode(MotorPinsRight[i], OUTPUT);
+    digitalWrite(MotorPinsRight[i], LOW);
+  }
+  //Second motor pin initializations
+  for (int i = 0; i < 4; i++){
+    pinMode(MotorPinsLeft[i], OUTPUT);
+    digitalWrite(MotorPinsLeft[i], LOW);
+  }
+  //
+  //pinMode(trigPin, OUTPUT);
+  //pinMode(echoPin, INPUT);
+  //Serial.begin(9600);
 }
 
 void loop() {
   // read the sensor value:
-  int sensorReading = analogRead(A0);
+  //int sensorReading = analogRead(A0);
   // map it to a range from 0 to 100:
-  int motorSpeed = 50; //= map(sensorReading, 0, 1023, 0, 100);
+  int motorSpeed = 100; //= map(sensorReading, 0, 1023, 0, 100);
   // set the motor speed:
   if (motorSpeed > 0) {
-    myStepper.setSpeed(motorSpeed);
+    //Move motor if signal on
+    //MoveSide(MotorPinsRight, 400);
+    MoveForward(400);
+    delay(2000);
+    TurnRight(400);
+    delay(2000);
+    TurnLeft(400);
+    //myStepper.setSpeed(motorSpeed);
     // step 1/100 of a revolution:
-    myStepper.step(stepsPerRevolution / 100);
+    //myStepper.step(stepsPerRevolution / 100);
   }
-  int distance = SonicSense();
-  Serial.print("Distance: ");
-  Serial.println(distance);
+  //int distance = SonicSense();
+  //Serial.print("Distance: ");
+  //Serial.println(distance);
+  delay(10000);
 }
 
 
