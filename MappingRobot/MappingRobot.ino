68b97db4ad369cbc6afc2611de6d484d60497379
bionickatana <nathan@hintonclan.org>
2025-03-01 09:45:03 -0700

    Working on integrating everything together

--- MappingRobot/MappingRobot.ino
+++ MappingRobot/MappingRobot.ino
@@ -1,144 +1,299 @@
 
 /*
- Stepper Motor Control - speed control
 
- This program drives a unipolar or bipolar stepper motor.
- The motor is attached to digital pins 8 - 11 of the Arduino.
- A potentiometer is connected to analog input 0.
-
- The motor will rotate in a clockwise direction. The higher the potentiometer value,
- the faster the motor speed. Because setSpeed() sets the delay between steps,
- you may notice the motor is less responsive to changes in the sensor value at
- low speeds.
-
- Created 30 Nov. 2009
- Modified 28 Oct 2010
- by Tom Igoe
+USU 2025 Hackathon
 
 Used:
-* HC-SR04 example sketch
+* HC-SR04 example sketch to get started with motors and then moved on
  *
  * https://create.arduino.cc/projecthub/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-036380
  *
  * by Isaac100
  */
- //Ultrasonic Sensors
-const int trigPin = 12;
-const int echoPin = 13;
-float duration_1, distance_1;
+ 
+ #include "I2Cdev.h"
+ #include "MPU6050_6Axis_MotionApps20.h"
+ 
+ #define DEBUG
+ 
+ #ifdef DEBUG
+ #define DEBUG_PRINT(x) Serial.println(x)
+ #else
+ #define DEBUG_PRINT(x)
+ #endif
+ 
+ /* SETUP FOR UPTRASONIC SENSORS */
+ #define FRONT_ULTRA_TRIGGER PIN_A0
+ #define FRONT_ULTRA_ECHO PIN_A1
+ #define RIGHT_ULTRA_TRIGGER PIN_A2
+ #define RIGHT_ULTRA_ECHO PIN_A3
+ #define LEFT_ULTRA_TRIGGER PIN_A4
+ #define LEFT_ULTRA_ECHO PIN_A5
+ 
+ unsigned long previous_ultra_millis = 0;
+ #define ultraSensorInterval 100
+ float frontUltraReading = 0;
+ float rightUltraReading = 0;
+ float  leftUltraReading = 0;
+ 
+ unsigned long previous_report_millis = 0;
+ #define report_interval 1000
+ 
+ /* Setup for the MPU6050 */
+ MPU6050 mpu;
+ 
+ int const MPU_INTERRUPT_PIN = 2; // Define the interruption #0 pin
+ long int xPosition = 0; // These should be measured in cm which should give us *plenty* of precision
+ long int yPosition = 0;
+ double report_angle = 0;
+ float currentRobotAngle = 0;
 
-int SonicSense(){
-  //Send out signal to request distance
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  //Read in pulse
-  int duration = pulseIn(echoPin, HIGH);
-  int distance = (duration*.0343)/2;
-  return distance;
-}
-
-#include <Stepper.h>
-
-const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
-// for your motor
-
-
-// initialize the stepper library on pins 8 through 11:
-const int MotorPinsRight[] = {8, 9, 10, 11};
-const int MotorPinsLeft[] = {7, 6, 5, 4};
-int Time_Delay = 3;//3 milliseconds
-void MoveSide(int motor[], int steps){
-  for (int j = 0; j < steps; j++){
-    for (int i = 0; i < 4; i++){
-      digitalWrite(motor[i], HIGH);
-      delay(Time_Delay);
-      digitalWrite(motor[i], LOW);
-    }
-  }
-}
-void MoveForward(int steps){
-  for (int j = 0; j < steps; j++){
-    for (int i = 0; i < 4; i++){
-      digitalWrite(MotorPinsRight[i], HIGH);
-      digitalWrite(MotorPinsLeft[i], HIGH);
-      delay(Time_Delay);
-      digitalWrite(MotorPinsRight[i], LOW);
-      digitalWrite(MotorPinsLeft[i], LOW);
-    }
-  }
-}
-void TurnLeft(int steps){
-  for (int n = 0; n < steps; n++){
-    int j = 3;
-    for (int i = 0; i < 4; i++){
-      digitalWrite(MotorPinsRight[j], HIGH);
-      digitalWrite(MotorPinsLeft[i], HIGH);
-      delay(Time_Delay);
-      digitalWrite(MotorPinsRight[j], LOW);
-      digitalWrite(MotorPinsLeft[i], LOW);
-      j -= 1;
-    }
-  }
-}
-void TurnRight(int steps){
-  for (int n = 0; n < steps; n++){
-    int j = 3;
-    for (int i = 0; i < 4; i++){
-      digitalWrite(MotorPinsRight[i], HIGH);
-      digitalWrite(MotorPinsLeft[j], HIGH);
-      delay(Time_Delay);
-      digitalWrite(MotorPinsRight[i], LOW);
-      digitalWrite(MotorPinsLeft[j], LOW);
-      j -= 1;
-    }
-  }
-}
-//Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-
-int stepCount = 0;  // number of steps the motor has taken
-
-void setup() {
-  //First motor pin initializations
-  for (int i = 0; i < 4; i++){
-    pinMode(MotorPinsRight[i], OUTPUT);
-    digitalWrite(MotorPinsRight[i], LOW);
-  }
-  //Second motor pin initializations
-  for (int i = 0; i < 4; i++){
-    pinMode(MotorPinsLeft[i], OUTPUT);
-    digitalWrite(MotorPinsLeft[i], LOW);
-  }
-  //
-  //pinMode(trigPin, OUTPUT);
-  //pinMode(echoPin, INPUT);
-  //Serial.begin(9600);
-}
-
-void loop() {
-  // read the sensor value:
-  //int sensorReading = analogRead(A0);
-  // map it to a range from 0 to 100:
-  int motorSpeed = 100; //= map(sensorReading, 0, 1023, 0, 100);
-  // set the motor speed:
-  if (motorSpeed > 0) {
-    //Move motor if signal on
-    //MoveSide(MotorPinsRight, 400);
-    MoveForward(400);
-    delay(2000);
-    TurnRight(400);
-    delay(2000);
-    TurnLeft(400);
-    //myStepper.setSpeed(motorSpeed);
-    // step 1/100 of a revolution:
-    //myStepper.step(stepsPerRevolution / 100);
-  }
-  //int distance = SonicSense();
-  //Serial.print("Distance: ");
-  //Serial.println(distance);
-  delay(10000);
-}
-
-
+ bool DMPReady = false;  // Set true if DMP init was successful
+ uint8_t MPUIntStatus;   // Holds actual interrupt status byte from MPU
+ uint8_t devStatus;      // Return status after each device operation (0 = success, !0 = error)
+ uint16_t packetSize;    // Expected DMP packet size (default is 42 bytes)
+ uint8_t FIFOBuffer[64]; // FIFO storage buffer
+ Quaternion q;
+ VectorInt16 aa;
+ VectorInt16 aaReal;
+ VectorFloat gravity;
+ float ypr[3];
+  
+ volatile bool MPUInterrupt = false;     // Indicates whether MPU6050 interrupt pin has gone high
+ void DMPDataReady() {
+   MPUInterrupt = true;
+ }
+ 
+ long int robotAngle = 0;
+ long int robotLastReportAngle = 0;
+ 
+ /* MOTOR SETUP */
+ const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+ long int robotMotorSteps = 0;
+ long int robotMotorLastReportSteps = 0;
+ 
+ // Setup the stepper motors for usage
+ const int MotorPinsRight[] = {8, 9, 10, 11};
+ const int MotorPinsLeft[] = {7, 6, 5, 4};
+ int Time_Delay = 3;//3 milliseconds
+ 
+ float read_ultrasonic(int trigger, int echo);
+ void error_blink(void);
+ void MoveSide(int motor[], int steps);
+ void MoveForward(int steps);
+ void TurnLeft(int steps);
+ void TurnRight(int steps);
+ 
+ 
+ /// @brief Setup the sensors and motors
+ void setup() {
+   // Setup light for error
+   pinMode(13, OUTPUT);
+ 
+   /* SETUP MOTORS */
+   // First motor pin initializations
+   for (int i = 0; i < 4; i++){
+     pinMode(MotorPinsRight[i], OUTPUT);
+     digitalWrite(MotorPinsRight[i], LOW);
+   }
+   // Second motor pin initializations
+   for (int i = 0; i < 4; i++){
+     pinMode(MotorPinsLeft[i], OUTPUT);
+     digitalWrite(MotorPinsLeft[i], LOW);
+   }
+ 
+   /* Setup the ultrasonic sensors: */
+   pinMode(FRONT_ULTRA_TRIGGER, OUTPUT);
+   pinMode(FRONT_ULTRA_ECHO, INPUT);
+   pinMode(RIGHT_ULTRA_TRIGGER, OUTPUT);
+   pinMode(RIGHT_ULTRA_ECHO, INPUT);
+   pinMode(LEFT_ULTRA_TRIGGER, OUTPUT);
+   pinMode(LEFT_ULTRA_ECHO, INPUT);
+ 
+   /* SETUP THE MPU */
+   digitalWrite(13, HIGH);
+   delay(500);
+   digitalWrite(13, LOW);
+   delay(4500); // wait a while for the robot to be stable before initalizing stuff
+   digitalWrite(13, HIGH); // Write debug light high, if there is a problem it will blink.
+   Wire.begin();
+   Wire.setClock(400000); // 400kHz I2C clock. Comment on this line if having compilation difficulties
+   Serial.begin(115200);
+   while (!Serial);
+ 
+   // initialize the MPU device
+   DEBUG_PRINT("initalizing I2C devices");
+   mpu.initialize();
+   Serial.println("test");
+   pinMode(MPU_INTERRUPT_PIN, INPUT);
+ 
+   DEBUG_PRINT("Testing connection to MPU6050");
+   if (mpu.testConnection() == false) {
+     Serial.println("FAILED TO CONNECT TO MPU6050!!!");
+     // Blink the light if there is an error
+     error_blink();
+   } else {
+     DEBUG_PRINT("Connection success!");
+   }
+ 
+   devStatus = mpu.dmpInitialize();
+ 
+   // add gyro offsets
+   mpu.setXGyroOffset(0);
+   mpu.setYGyroOffset(0);
+   mpu.setZGyroOffset(0);
+   mpu.setXAccelOffset(0);
+   mpu.setYAccelOffset(0);
+   mpu.setZAccelOffset(0);
+ 
+   // Make sure the initalization worked:
+   if (devStatus == 0) {
+     mpu.CalibrateAccel(6); // the calibration time.
+     mpu.CalibrateGyro(6);
+     DEBUG_PRINT("Enabling DMP");
+     mpu.setDMPEnabled(true);
+ 
+     // Enable the arduino interupt on pin 2:
+     attachInterrupt(digitalPinToInterrupt(MPU_INTERRUPT_PIN), DMPDataReady, RISING);
+     MPUIntStatus = mpu.getIntStatus();
+ 
+     // Set the dmp ready flag to allow the main loop to run:
+     DMPReady = true;
+     packetSize = mpu.dmpGetFIFOPacketSize();
+   } else {
+     Serial.print("SMP initalization failed with code: ");
+     Serial.println(devStatus);
+   }
+ }
+ 
+ /// @brief Main look for the robot
+ void loop() {
+   // Sample the mpu:
+   if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) {
+     mpu.dmpGetQuaternion(&q, FIFOBuffer);
+     mpu.dmpGetGravity(&gravity, &q);
+     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+     currentRobotAngle = ypr[0]; // the yaw of the sensor (This is in radians)
+   }
+ 
+   // Sample the ultrasonic sensors
+   unsigned long currentMillis = millis();
+   if (currentMillis - previous_ultra_millis >= ultraSensorInterval) {
+     // Save the last time the function was called
+     previous_ultra_millis = currentMillis;
+     frontUltraReading = read_ultrasonic(FRONT_ULTRA_TRIGGER, FRONT_ULTRA_ECHO);
+     rightUltraReading = read_ultrasonic(RIGHT_ULTRA_TRIGGER, RIGHT_ULTRA_ECHO);
+     leftUltraReading = read_ultrasonic(LEFT_ULTRA_TRIGGER, LEFT_ULTRA_ECHO);
+   }
+ 
+   /* Send the data to the PI */
+   if (currentMillis - previous_report_millis >= report_interval) {
+     previous_report_millis = currentMillis;
+     // Print out lines to pi
+     Serial.print(frontUltraReading, 2);
+     Serial.print(", ");
+     Serial.print(rightUltraReading, 2);
+     Serial.print(", ");
+     Serial.print(leftUltraReading, 2);
+     Serial.print(", ");
+     Serial.print(report_angle * 180/M_PI);
+     Serial.print(", ");
+     Serial.print(robotMotorSteps);
+   }
+ 
+   if (frontUltraReading > 10) {
+     MoveForward(515);
+     delay(3000);
+   }
+ }
+ 
+ /// @brief Move one side of the wheels
+ /// @param motor The motor/set of motors to move
+ /// @param steps The number of steps to move
+ void MoveSide(int motor[], int steps){
+   for (int j = 0; j < steps; j++){
+     for (int i = 0; i < 4; i++){
+       digitalWrite(motor[i], HIGH);
+       delay(Time_Delay);
+       digitalWrite(motor[i], LOW);
+     }
+   }
+ }
+ 
+ /// @brief Move the robot forward
+ /// @param steps Move the robot forward a number of encoder counts.
+ void MoveForward(int steps){
+   //accumulate in global where the steps is
+   for (int j = 0; j < steps; j++){
+     for (int i = 0; i < 4; i++){
+       digitalWrite(MotorPinsRight[i], HIGH);
+       digitalWrite(MotorPinsLeft[i], HIGH);
+       delay(Time_Delay);
+       digitalWrite(MotorPinsRight[i], LOW);
+       digitalWrite(MotorPinsLeft[i], LOW);
+     }
+   }
+ }
+ 
+ /// @brief Turn the robot to the left
+ /// @param steps The number of degrees to turn. This will use feedback from the gyro sensor
+ void TurnLeft(int steps){
+   for (int n = 0; n < steps; n++){
+     int j = 3;
+     for (int i = 0; i < 4; i++){
+       digitalWrite(MotorPinsRight[j], HIGH);
+       digitalWrite(MotorPinsLeft[i], HIGH);
+       delay(Time_Delay);
+       digitalWrite(MotorPinsRight[j], LOW);
+       digitalWrite(MotorPinsLeft[i], LOW);
+       j -= 1;
+     }
+   }
+ }
+ 
+ /// @brief Turn the robot to the right
+ /// @param steps The number of degrees to turn. This will use feedback from the gyro sensor
+ void TurnRight(int steps){
+   for (int n = 0; n < steps; n++){
+     int j = 3;
+     for (int i = 0; i < 4; i++){
+       digitalWrite(MotorPinsRight[i], HIGH);
+       digitalWrite(MotorPinsLeft[j], HIGH);
+       delay(Time_Delay);
+       digitalWrite(MotorPinsRight[i], LOW);
+       digitalWrite(MotorPinsLeft[j], LOW);
+       j -= 1;
+     }
+   }
+ }
+ 
+ /// @brief Read an ultrasonic sensor
+ /// @param trigger The trigger pin for the sensor
+ /// @param echo  The echo pin for the sensor
+ /// @return The distance that the sensor measured
+ float read_ultrasonic(int trigger, int echo) {
+   float avg = 0;
+   for (int ii = 0; ii < 3; ii++) {
+     Serial.println(ii);
+     digitalWrite(trigger, LOW);
+     delayMicroseconds(2);
+     digitalWrite(trigger, HIGH);
+     delayMicroseconds(10);
+     digitalWrite(trigger, LOW);
+     unsigned long duration = pulseIn(echo, HIGH);
+     avg += (duration*.0343)/2;
+   }
+   return avg/3;
+ }
+ 
+ /// @brief Blinks the onboard led if there is an error
+ void error_blink() {
+   while (true){
+     digitalWrite(13, LOW);
+     delay(250);
+     digitalWrite(13, HIGH);
+     delay(250);
+   }
+ }
+    
+ 
--- sensors/src/main.cpp
+++ sensors/src/main.cpp
@@ -1,188 +1,299 @@
-// Nathan Hinton
-// USU 2025 Hackathon
-// Sensors file
-// This will take the sensors and then create a fusion of them that will be pushed out to a raspberry pi which will log the data for later processing
 
-// MPU code based on the DMP6 example from the MPU6050 library
+/*
 
-#include "I2Cdev.h"
-#include "MPU6050_6Axis_MotionApps20.h"
+USU 2025 Hackathon
 
-#define DEBUG
+Used:
+* HC-SR04 example sketch to get started with motors and then moved on
+ *
+ * https://create.arduino.cc/projecthub/Isaac100/getting-started-with-the-hc-sr04-ultrasonic-sensor-036380
+ *
+ * by Isaac100
+ */
+ 
+ #include "I2Cdev.h"
+ #include "MPU6050_6Axis_MotionApps20.h"
+ 
+ #define DEBUG
+ 
+ #ifdef DEBUG
+ #define DEBUG_PRINT(x) Serial.println(x)
+ #else
+ #define DEBUG_PRINT(x)
+ #endif
+ 
+ /* SETUP FOR UPTRASONIC SENSORS */
+ #define FRONT_ULTRA_TRIGGER PIN_A0
+ #define FRONT_ULTRA_ECHO PIN_A1
+ #define RIGHT_ULTRA_TRIGGER PIN_A2
+ #define RIGHT_ULTRA_ECHO PIN_A3
+ #define LEFT_ULTRA_TRIGGER PIN_A4
+ #define LEFT_ULTRA_ECHO PIN_A5
+ 
+ unsigned long previous_ultra_millis = 0;
+ #define ultraSensorInterval 100
+ float frontUltraReading = 0;
+ float rightUltraReading = 0;
+ float  leftUltraReading = 0;
+ 
+ unsigned long previous_report_millis = 0;
+ #define report_interval 1000
+ 
+ /* Setup for the MPU6050 */
+ MPU6050 mpu;
+ 
+ int const MPU_INTERRUPT_PIN = 2; // Define the interruption #0 pin
+ long int xPosition = 0; // These should be measured in cm which should give us *plenty* of precision
+ long int yPosition = 0;
+ double report_angle = 0;
+ float currentRobotAngle = 0;
 
-#ifdef DEBUG
-#define DEBUG_PRINT(x) Serial.println(x)
-#else
-#define DEBUG_PRINT(x)
-#endif
-
-/* SETUP FOR UPTRASONIC SENSORS */
-#define FRONT_ULTRA_TRIGGER PIN3
-#define FRONT_ULTRA_ECHO PIN_A3
-
-unsigned long previous_ultra_millis = 0;
-#define ultra_sensor_interval 100
-float ultra1_reading = 0;
-float ultra2_reading = 0;
-float ultra3_reading = 0;
-
-unsigned long previous_report_millis = 0;
-#define report_interval 1000
-
-/* Setup for the MPU6050 */
-MPU6050 mpu;
-
-int const MPU_INTERRUPT_PIN = 2; // Define the interruption #0 pin
-long int xPosition = 0; // These should be measured in cm which should give us *plenty* of precision
-long int yPosition = 0;
-double report_angle = 0;
-
-bool DMPReady = false;  // Set true if DMP init was successful
-uint8_t MPUIntStatus;   // Holds actual interrupt status byte from MPU
-uint8_t devStatus;      // Return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize;    // Expected DMP packet size (default is 42 bytes)
-uint8_t FIFOBuffer[64]; // FIFO storage buffer
-Quaternion q;
-VectorInt16 aa;
-VectorInt16 aaReal;
-VectorFloat gravity;
-float ypr[3];
-
-volatile bool MPUInterrupt = false;     // Indicates whether MPU6050 interrupt pin has gone high
-void DMPDataReady() {
-  MPUInterrupt = true;
-}
-
-float read_ultrasonic(int trigger, int echo);
-void error_blink(void);
-
-void setup() {
-  pinMode(13, OUTPUT);
-  // digitalWrite(13, HIGH);
-  // delay(500);
-  // digitalWrite(13, LOW);
-  // delay(4500); // wait a while for the robot to be stable before initalizing stuff
-  // digitalWrite(13, HIGH); // Write debug light high, if there is a problem it will blink.
-  Wire.begin();
-  Wire.setClock(400000); // 400kHz I2C clock. Comment on this line if having compilation difficulties
-  Serial.begin(115200);
-  while (!Serial);
-
-  // initialize the MPU device
-  DEBUG_PRINT("initalizing I2C devices");
-  mpu.initialize();
-  Serial.println("test");
-  pinMode(MPU_INTERRUPT_PIN, INPUT);
-
-  DEBUG_PRINT("Testing connection to MPU6050");
-  if (mpu.testConnection() == false) {
-    Serial.println("FAILED TO CONNECT TO MPU6050!!!");
-    // Blink the light if there is an error
-    error_blink();
-  } else {
-    DEBUG_PRINT("Connection success!");
-  }
-
-  devStatus = mpu.dmpInitialize();
-
-  // add gyro offsets
-  mpu.setXGyroOffset(0);
-  mpu.setYGyroOffset(0);
-  mpu.setZGyroOffset(0);
-  mpu.setXAccelOffset(0);
-  mpu.setYAccelOffset(0);
-  mpu.setZAccelOffset(0);
-
-  // Make sure the initalization worked:
-  if (devStatus == 0) {
-    mpu.CalibrateAccel(6); // the calibration time.
-    mpu.CalibrateGyro(6);
-    DEBUG_PRINT("Enabling DMP");
-    mpu.setDMPEnabled(true);
-
-    // Enable the arduino interupt on pin 2:
-    attachInterrupt(digitalPinToInterrupt(MPU_INTERRUPT_PIN), DMPDataReady, RISING);
-    MPUIntStatus = mpu.getIntStatus();
-
-    // Set the dmp ready flag to allow the main loop to run:
-    DMPReady = true;
-    packetSize = mpu.dmpGetFIFOPacketSize();
-  } else{
-    Serial.print("SMP initalization failed with code: ");
-    Serial.println(devStatus);
+ bool DMPReady = false;  // Set true if DMP init was successful
+ uint8_t MPUIntStatus;   // Holds actual interrupt status byte from MPU
+ uint8_t devStatus;      // Return status after each device operation (0 = success, !0 = error)
+ uint16_t packetSize;    // Expected DMP packet size (default is 42 bytes)
+ uint8_t FIFOBuffer[64]; // FIFO storage buffer
+ Quaternion q;
+ VectorInt16 aa;
+ VectorInt16 aaReal;
+ VectorFloat gravity;
+ float ypr[3];
+  
+ volatile bool MPUInterrupt = false;     // Indicates whether MPU6050 interrupt pin has gone high
+ void DMPDataReady() {
+   MPUInterrupt = true;
+ }
+ 
+ long int robotAngle = 0;
+ long int robotLastReportAngle = 0;
+ 
+ /* MOTOR SETUP */
+ const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
+ long int robotMotorSteps = 0;
+ long int robotMotorLastReportSteps = 0;
+ 
+ // Setup the stepper motors for usage
+ const int MotorPinsRight[] = {8, 9, 10, 11};
+ const int MotorPinsLeft[] = {7, 6, 5, 4};
+ int Time_Delay = 3;//3 milliseconds
+ 
+ float read_ultrasonic(int trigger, int echo);
+ void error_blink(void);
+ void MoveSide(int motor[], int steps);
+ void MoveForward(int steps);
+ void TurnLeft(int steps);
+ void TurnRight(int steps);
+ 
+ 
+ /// @brief Setup the sensors and motors
+ void setup() {
+   // Setup light for error
+   pinMode(13, OUTPUT);
+ 
+   /* SETUP MOTORS */
+   // First motor pin initializations
+   for (int i = 0; i < 4; i++){
+     pinMode(MotorPinsRight[i], OUTPUT);
+     digitalWrite(MotorPinsRight[i], LOW);
+   }
+   // Second motor pin initializations
+   for (int i = 0; i < 4; i++){
+     pinMode(MotorPinsLeft[i], OUTPUT);
+     digitalWrite(MotorPinsLeft[i], LOW);
+   }
+ 
+   /* Setup the ultrasonic sensors: */
+   pinMode(FRONT_ULTRA_TRIGGER, OUTPUT);
+   pinMode(FRONT_ULTRA_ECHO, INPUT);
+   pinMode(RIGHT_ULTRA_TRIGGER, OUTPUT);
+   pinMode(RIGHT_ULTRA_ECHO, INPUT);
+   pinMode(LEFT_ULTRA_TRIGGER, OUTPUT);
+   pinMode(LEFT_ULTRA_ECHO, INPUT);
+ 
+   /* SETUP THE MPU */
+   digitalWrite(13, HIGH);
+   delay(500);
+   digitalWrite(13, LOW);
+   delay(4500); // wait a while for the robot to be stable before initalizing stuff
+   digitalWrite(13, HIGH); // Write debug light high, if there is a problem it will blink.
+   Wire.begin();
+   Wire.setClock(400000); // 400kHz I2C clock. Comment on this line if having compilation difficulties
+   Serial.begin(115200);
+   while (!Serial);
+ 
+   // initialize the MPU device
+   DEBUG_PRINT("initalizing I2C devices");
+   mpu.initialize();
+   Serial.println("test");
+   pinMode(MPU_INTERRUPT_PIN, INPUT);
+ 
+   DEBUG_PRINT("Testing connection to MPU6050");
+   if (mpu.testConnection() == false) {
+     Serial.println("FAILED TO CONNECT TO MPU6050!!!");
+     // Blink the light if there is an error
+     error_blink();
+   } else {
+     DEBUG_PRINT("Connection success!");
+   }
+ 
+   devStatus = mpu.dmpInitialize();
+ 
+   // add gyro offsets
+   mpu.setXGyroOffset(0);
+   mpu.setYGyroOffset(0);
+   mpu.setZGyroOffset(0);
+   mpu.setXAccelOffset(0);
+   mpu.setYAccelOffset(0);
+   mpu.setZAccelOffset(0);
+ 
+   // Make sure the initalization worked:
+   if (devStatus == 0) {
+     mpu.CalibrateAccel(6); // the calibration time.
+     mpu.CalibrateGyro(6);
+     DEBUG_PRINT("Enabling DMP");
+     mpu.setDMPEnabled(true);
+ 
+     // Enable the arduino interupt on pin 2:
+     attachInterrupt(digitalPinToInterrupt(MPU_INTERRUPT_PIN), DMPDataReady, RISING);
+     MPUIntStatus = mpu.getIntStatus();
+ 
+     // Set the dmp ready flag to allow the main loop to run:
+     DMPReady = true;
+     packetSize = mpu.dmpGetFIFOPacketSize();
+   } else {
+     Serial.print("SMP initalization failed with code: ");
+     Serial.println(devStatus);
+   }
+ }
+ 
+ /// @brief Main look for the robot
+ void loop() {
+   // Sample the mpu:
+   if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) {
+     mpu.dmpGetQuaternion(&q, FIFOBuffer);
+     mpu.dmpGetGravity(&gravity, &q);
+     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+     currentRobotAngle = ypr[0]; // the yaw of the sensor (This is in radians)
+   }
+ 
+   // Sample the ultrasonic sensors
+   unsigned long currentMillis = millis();
+   if (currentMillis - previous_ultra_millis >= ultraSensorInterval) {
+     // Save the last time the function was called
+     previous_ultra_millis = currentMillis;
+     frontUltraReading = read_ultrasonic(FRONT_ULTRA_TRIGGER, FRONT_ULTRA_ECHO);
+     rightUltraReading = read_ultrasonic(RIGHT_ULTRA_TRIGGER, RIGHT_ULTRA_ECHO);
+     leftUltraReading = read_ultrasonic(LEFT_ULTRA_TRIGGER, LEFT_ULTRA_ECHO);
+   }
+ 
+   /* Send the data to the PI */
+   if (currentMillis - previous_report_millis >= report_interval) {
+     previous_report_millis = currentMillis;
+     // Print out lines to pi
+     Serial.print(frontUltraReading, 2);
+     Serial.print(", ");
+     Serial.print(rightUltraReading, 2);
+     Serial.print(", ");
+     Serial.print(leftUltraReading, 2);
+     Serial.print(", ");
+     Serial.print(report_angle * 180/M_PI);
+     Serial.print(", ");
+     Serial.print(robotMotorSteps);
+   }
+ 
+   if (frontUltraReading > 10) {
+     MoveForward(515);
+     delay(3000);
+   }
+ }
+ 
+ /// @brief Move one side of the wheels
+ /// @param motor The motor/set of motors to move
+ /// @param steps The number of steps to move
+ void MoveSide(int motor[], int steps){
+   for (int j = 0; j < steps; j++){
+     for (int i = 0; i < 4; i++){
+       digitalWrite(motor[i], HIGH);
+       delay(Time_Delay);
+       digitalWrite(motor[i], LOW);
+     }
+   }
+ }
+ 
+ /// @brief Move the robot forward
+ /// @param steps Move the robot forward a number of encoder counts.
+ void MoveForward(int steps){
+   //accumulate in global where the steps is
+   for (int j = 0; j < steps; j++){
+     for (int i = 0; i < 4; i++){
+       digitalWrite(MotorPinsRight[i], HIGH);
+       digitalWrite(MotorPinsLeft[i], HIGH);
+       delay(Time_Delay);
+       digitalWrite(MotorPinsRight[i], LOW);
+       digitalWrite(MotorPinsLeft[i], LOW);
+     }
+   }
+ }
+ 
+ /// @brief Turn the robot to the left
+ /// @param steps The number of degrees to turn. This will use feedback from the gyro sensor
+ void TurnLeft(int steps){
+   for (int n = 0; n < steps; n++){
+     int j = 3;
+     for (int i = 0; i < 4; i++){
+       digitalWrite(MotorPinsRight[j], HIGH);
+       digitalWrite(MotorPinsLeft[i], HIGH);
+       delay(Time_Delay);
+       digitalWrite(MotorPinsRight[j], LOW);
+       digitalWrite(MotorPinsLeft[i], LOW);
+       j -= 1;
+     }
+   }
+ }
+ 
+ /// @brief Turn the robot to the right
+ /// @param steps The number of degrees to turn. This will use feedback from the gyro sensor
+ void TurnRight(int steps){
+   for (int n = 0; n < steps; n++){
+     int j = 3;
+     for (int i = 0; i < 4; i++){
+       digitalWrite(MotorPinsRight[i], HIGH);
+       digitalWrite(MotorPinsLeft[j], HIGH);
+       delay(Time_Delay);
+       digitalWrite(MotorPinsRight[i], LOW);
+       digitalWrite(MotorPinsLeft[j], LOW);
+       j -= 1;
+     }
+   }
+ }
+ 
+ /// @brief Read an ultrasonic sensor
+ /// @param trigger The trigger pin for the sensor
+ /// @param echo  The echo pin for the sensor
+ /// @return The distance that the sensor measured
+ float read_ultrasonic(int trigger, int echo) {
+   float avg = 0;
+   for (int ii = 0; ii < 3; ii++) {
+     Serial.println(ii);
+     digitalWrite(trigger, LOW);
+     delayMicroseconds(2);
+     digitalWrite(trigger, HIGH);
+     delayMicroseconds(10);
+     digitalWrite(trigger, LOW);
+     unsigned long duration = pulseIn(echo, HIGH);
+     avg += (duration*.0343)/2;
+   }
+   return avg/3;
+ }
+ 
+ /// @brief Blinks the onboard led if there is an error
+ void error_blink() {
+   while (true){
+     digitalWrite(13, LOW);
+     delay(250);
+     digitalWrite(13, HIGH);
+     delay(250);
+   }
+ }
     
-    // initialize the ultrasonic sensors:
-    pinMode(FRONT_ULTRA_TRIGGER, OUTPUT);
-    pinMode(FRONT_ULTRA_ECHO, INPUT);
-  }
-}
-
-void loop() {
-  // Sample the mpu:
-  if (mpu.dmpGetCurrentFIFOPacket(FIFOBuffer)) {
-    mpu.dmpGetQuaternion(&q, FIFOBuffer);
-    mpu.dmpGetAccel(&aa, FIFOBuffer);
-    mpu.dmpGetGravity(&gravity, &q);
-    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    report_angle = ypr[0]; // the yaw of the sensor
-    Serial.print("areal\t");
-    Serial.print(aaReal.x);
-    Serial.print("\t");
-    Serial.print(aaReal.y);
-    Serial.print("\t");
-    Serial.println(aaReal.z);    xPosition += cos(report_angle) * aaReal.x;
-    yPosition += sin(report_angle) * aaReal.x;
-  }
-  // Sample the ultrasonic sensor
-  unsigned long currentMillis = millis();
-  if (currentMillis - previous_ultra_millis >= ultra_sensor_interval) {
-    // Save the last time the function was called
-    previous_ultra_millis = currentMillis;
-    ultra1_reading += read_ultrasonic(FRONT_ULTRA_TRIGGER, FRONT_ULTRA_ECHO);
-  }
-  if (currentMillis - previous_report_millis >= report_interval) {
-    previous_report_millis = currentMillis;
-    // Print out lines to pi
-    Serial.print(ultra1_reading/(report_interval/ultra_sensor_interval), 2);
-    Serial.print(", ");
-    Serial.print(ultra2_reading/(report_interval/ultra_sensor_interval), 2);
-    Serial.print(", ");
-    Serial.print(ultra3_reading/(report_interval/ultra_sensor_interval), 2);
-    Serial.print(", ");
-    Serial.print(report_angle * 180/M_PI);
-    Serial.print(", ");
-    Serial.print(xPosition);
-    Serial.print(", ");
-    Serial.print(yPosition);
-    Serial.println();
-    // Reset report variables
-    ultra1_reading = 0;
-  }
-}
-
-
-/// @brief Read an ultrasonic sensor
-/// @param trigger The trigger pin for the sensor
-/// @param echo  The echo pin for the sensor
-/// @return The distance that the sensor measured
-float read_ultrasonic(int trigger, int echo) {
-  digitalWrite(trigger, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigger, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigger, LOW);
-  unsigned long duration = pulseIn(echo, HIGH);
-  float distance = (duration*.0343)/2;
-  return distance;
-}
-
-void error_blink() {
-  while (true){
-    digitalWrite(13, LOW);
-    delay(250);
-    digitalWrite(13, HIGH);
-    delay(250);
-  }
-}
-   
+ 
